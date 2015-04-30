ccaaea527ae193264e13c24a81602cab5cf7fe0b
Ruan Nicolini <ruannicolini@gmail.com>
2015-04-30 11:51:39 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/DiSalgados/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/DiSalgados/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\DiSalgados/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\DiSalgados/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\DiSalgados/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\DiSalgados/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\DiSalgados/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\DiSalgados/image/');
+define('DIR_CACHE', 'C:\wamp\www\DiSalgados/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\DiSalgados/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\DiSalgados/system/upload/');
+define('DIR_MODIFICATION', 'C:\wamp\www\DiSalgados/system/modification/');
+define('DIR_LOGS', 'C:\wamp\www\DiSalgados/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'disalgados');
+define('DB_PREFIX', 'oc_');
